8574ace131cd50935df53b508d53a7899cc54183
Arash Haratian <arashharatian@hotmail.com>
2018-12-17 20:14:38 +0330

    Commiting for first time

---
+++ matrixLib.cpp
@@ -0,0 +1,315 @@
+#include "matrixLib.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iterator>
+
+using namespace std;
+//------------------------------Public Matrix(row,column) Constructor----------------
+Matrix::Matrix (int row, int column) : matrix_( vector<vector<double >>(row,vector<double >(column)) )
+{
+    this->row_=row;
+    this->column_=column;
+}
+//------------------------------Public Matrix(File) Constructor------------
+Matrix::Matrix (string address)
+{
+    this->row_ = this->column_ = 0;
+    size_t thisDelim = 0;
+    ifstream matrixfile;
+    matrixfile.open (address);
+    if(matrixfile.is_open())
+    {
+        string line = "";
+        while (getline(matrixfile, line) && line != "")
+        {
+            while (line.find(',', thisDelim + 1) && row_ == 0)
+            {
+                thisDelim = line.find(',', thisDelim + 1);
+                if (thisDelim == string::npos)
+                {
+                    column_++;
+                    break;
+                }
+                column_++;
+            }
+            row_++;
+        }
+    }
+    this->matrix_ = vector<vector<double > > (row_,vector<double >(column_));
+    this->openFile_(address);
+    matrixfile.close();
+}
+//------------------------------Overloading CIN---------------------------------
+istream& operator>>(istream &in, Matrix &matrix1)
+{
+    for(int it = 0 ; it < matrix1.row_ ; it++)
+    {
+        cout<< it+1 <<"  row:\n" ;
+        for ( int innerIt = 0 ; innerIt < matrix1.column_ ; innerIt++ )
+            in >> matrix1.matrix_.at(it).at(innerIt);
+    }
+}
+//------------------------------Output the this->matrix---------------------------
+void Matrix::output()
+{
+    for (int it = 0; it < this->row_; it++)
+    {
+        for(int innerIt = 0; innerIt < this->column_; innerIt++)
+            cout << this->matrix_.at(it).at(innerIt) << '\t';
+        cout << '\n';
+    }
+}
+//---------------------------Overloading SUM------------------------
+Matrix Matrix::operator+ (Matrix const &matrix)
+{
+    if(this->column_ == matrix.column_ && this->row_ == matrix.row_)
+    {
+        Matrix totalMartix (this->row_, this->column_);
+        for (int it = 0; it < this->row_; it++)
+            for (int innerIt = 0; innerIt < this->column_; innerIt++)
+                totalMartix.matrix_.at(it).at(innerIt) = this->matrix_.at(it).at(innerIt) + matrix.matrix_.at(it).at(innerIt);
+        return totalMartix;
+    }
+    else
+        cout << "ERROR!!!\n"
+                "THESE TWO MATRICES ARE NOT IN A SAME SHAPE.\n";
+}
+//-------------------------Overloading Multiply With Another Matrix -----------------------
+Matrix Matrix::operator* (Matrix const &matrix)
+{
+    if (this->column_ == matrix.row_)
+    {
+        Matrix productMatrix(this->row_, matrix.column_);
+        for (int it = 0; it < this->row_; it++)
+            for(int firstInnerIt = 0; firstInnerIt < matrix.column_; firstInnerIt++)
+                for (int secondInnerIt = 0; secondInnerIt < this->column_; secondInnerIt++)
+                {
+                    productMatrix.matrix_.at(it).at(firstInnerIt) += this->matrix_.at(it).at(secondInnerIt) * matrix.matrix_.at(secondInnerIt).at(firstInnerIt);
+                }
+        return productMatrix;
+    }
+    else
+        cout <<"ERROR!!!\n"
+               "COLUMN OF FIRST MATRIX NOT EQUAL TO ROW OF SECOND.\n";
+}
+//-------------------------Overloading Multiply With Number----------------------
+Matrix Matrix::operator*(int const coefficient)
+{
+    Matrix productMatrix(this->row_, this->column_);
+    for (int it = 0; it < this->row_; it++)
+        for (int innerIt = 0; innerIt < this->column_; innerIt++)
+            productMatrix.matrix_.at(it).at(innerIt) = this->matrix_.at(it).at(innerIt) * coefficient;
+    return productMatrix;
+}
+//--------------------------- int * matrix---------------------------
+Matrix operator * (int const coefficient, Matrix const matrix)
+{
+    Matrix productMatrix(matrix.row_, matrix.column_);
+    for (int it = 0; it < matrix.row_; it++)
+        for (int innerIt = 0; innerIt < matrix.column_; innerIt++)
+            productMatrix.matrix_.at(it).at(innerIt) = matrix.matrix_.at(it).at(innerIt) * coefficient;
+    return productMatrix;
+}
+//----------------------------Overloading Minus----------------------
+Matrix Matrix::operator - (Matrix const matrix)
+{
+    if(this->row_ == matrix.row_ && this->column_ == matrix.column_)
+    {
+        Matrix minusMatrix (this->row_, this->column_);
+        for (int it = 0; it < this->row_; it++)
+            for (int innerIt = 0; innerIt < this->column_; innerIt++)
+                minusMatrix.matrix_.at(it).at(innerIt) = this->matrix_.at(it).at(innerIt) - matrix.matrix_.at(it).at(innerIt);
+        return minusMatrix;
+    }
+    else
+        cout << "ERROR!!!\n"
+                "THESE TWO MATRICES ARE NOT IN A SAME SHAPE.\n";
+}
+//----------------------------- overloading == -----------------------
+bool Matrix ::operator==(Matrix const matrix)
+{
+    if(this->row_ == matrix.row_ && this->column_ == matrix.column_)
+    {
+        for (int it = 0; it < this->row_; it++)
+            for (int innerIt = 0; innerIt < this->column_; innerIt++)
+                if (this->matrix_.at (it).at (innerIt) != matrix.matrix_.at (it).at (innerIt))
+                    return false;
+        return true;
+    }
+    else
+        cout << "ERROR!!!\n"
+                "THESE TWO MATRICES ARE NOT IN A SAME SHAPE.\n";
+}
+//----------------------------Transposing----------------------------
+Matrix Matrix::transpose()
+{
+    Matrix transposeMatrix (this->column_, this->row_);
+    for (int it = 0; it < this->row_; it++)
+        for (int innerIt = 0; innerIt < this->column_; innerIt++)
+            transposeMatrix.matrix_.at(innerIt).at(it) = this->matrix_.at(it).at(innerIt);
+    return transposeMatrix;
+}
+//-------------------------Cofactor_-----------------------------
+void Matrix::getCofactor_(Matrix matrix1, Matrix &tempMatrix, int p, int q, int n)
+{
+    int i = 0, j = 0;
+    for (int row = 0; row < n; row++)
+    {
+        for (int column = 0; column < n; column++)
+        {
+            if (row != p && column != q)
+            {
+                tempMatrix.matrix_.at(i).at(j++) = matrix1.matrix_.at(row).at(column);
+                if (j == n - 1)
+                {
+                    j = 0;
+                    i++;
+                }
+            }
+        }
+    }
+}
+//----------------------private determinant_ -----------------------------
+double Matrix::determinant_ (int n)
+{
+    double determ = 0;
+    if (n == 1)
+        return this->matrix_.at(0).at(0);
+
+    Matrix tempMatrix (this->row_, this->row_);
+
+    int sign = 1;
+    for (int f = 0; f < n; f++)
+    {
+        getCofactor_(*this, tempMatrix, 0, f, n);
+        determ += sign * this->matrix_.at(0).at(f) * tempMatrix.determinant_ (n-1);
+        sign = -sign;
+    }
+    return determ;
+}
+//-------------------Public determinant------------------------------------
+double Matrix::determinant()
+{
+    return this->determinant_(this->row_);
+}
+//-----------------Elements Textfile To Matrix----------------------
+void Matrix::openFile_(string address)
+{
+    size_t previousDelim = -1;
+    size_t thisDelim = 0;
+    ifstream elementFile (address);
+    if (elementFile.is_open())
+    {
+        string line = "";
+        int it = 0;
+        while (getline (elementFile, line) && line != "")
+        {
+            int innerIt = 0;
+            while (line.find (',', thisDelim + 1))
+            {
+                thisDelim = line.find(',', thisDelim + 1);
+                if (thisDelim == string::npos)
+                {
+                    thisDelim = line.find('\n');
+                    if (line [previousDelim + 1] == '-')
+                        this->matrix_.at(it).at(innerIt) = -stod (line.substr (previousDelim + 2, thisDelim));
+                    else
+                        this->matrix_.at(it).at(innerIt) = stod (line.substr (previousDelim + 1, thisDelim - 1));
+                    break;
+                }
+                if (line[previousDelim + 1] == '-')
+                {
+                    this->matrix_.at(it).at(innerIt) = -stod (line.substr (previousDelim + 2, thisDelim));
+                    innerIt++;
+                }
+                else
+                {
+                    this->matrix_.at(it).at(innerIt) = stod (line.substr (previousDelim + 1, thisDelim));
+                    innerIt++;
+                }
+                previousDelim = thisDelim;
+            }
+            it++;
+            previousDelim = -1;
+            thisDelim = 0;
+        }
+    }
+
+    else
+    {
+        cout<<"Can not open the file!!\n";
+        return;
+    }
+}
+//------------------------Saving Matrix To TextFile----------------------------------
+void Matrix::save(string name)
+{
+    ofstream outputFile (name);
+    for (int it = 0; it < this->row_; it++)
+    {
+        for (int innerIt = 0; innerIt < this->column_; innerIt++)
+        {
+            if (innerIt == this->column_ -1)
+                outputFile << this->matrix_.at(it).at(innerIt);
+            else
+                outputFile << this->matrix_.at(it).at(innerIt) << ",";
+        }
+        outputFile << '\n';
+    }
+}
+//-------------------------Get Row-------------------------------------
+int Matrix::getRow()
+{
+    return this->row_;
+}
+//---------------------Get column-------------------------------------
+int Matrix::getColumn()
+{
+    return this->column_;
+}//------------------- matrixSize-------------------------------------
+string Matrix::size()
+{
+    string size = "";
+    size = to_string(this->row_) + " , " + to_string(this->column_);
+    return  size;
+}
+//------------------private adjoint_ ------------------------------------------
+void Matrix::adjoint_(Matrix &adjoint)
+{
+    if (this->column_ == 1)
+    {
+        adjoint.matrix_.at(0).at(0) = 1;
+        return;
+    }
+    int sign = 1;
+    Matrix tempMatrix(this->row_, this->column_);
+
+    for (int  it= 0; it <this->row_; it++)
+        for (int innerIt = 0; innerIt< this->column_; innerIt++)
+        {
+            getCofactor_(*this, tempMatrix, it, innerIt, this->row_);
+            sign = ((it+innerIt)%2==0)? 1: -1;
+            int n = this->row_;
+            adjoint.matrix_.at(innerIt).at(it)= (sign) * (tempMatrix.determinant_(n-1));
+        }
+}
+//--------------------Public inverse ----------------------------------------
+Matrix Matrix::inverse()
+{
+    double determ = this->determinant_(this->column_);
+    if (determ == 0)
+        cout << "Singular matrix, can't find its inverse";
+    else
+    {
+        Matrix inverse(this->row_, this->column_);
+        Matrix adjoint(this->row_, this->column_);
+        this->adjoint_ (adjoint);
+        for (int it = 0; it < this->row_; it++)
+            for (int innerIt = 0; innerIt < this->column_; innerIt++)
+                inverse.matrix_.at(it).at(innerIt) = adjoint.matrix_.at(it).at(innerIt) / int(determ);
+        return inverse;
+    }
+}
---
+++ matrixLib.h
@@ -0,0 +1,51 @@
+
+//
+// Created by arashHaratian on 11/16/18.
+//
+
+#ifndef MATRIXLIBFINAL_LIBRARY_H
+#define MATRIXLIBFINAL_LIBRARY_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Matrix {
+private:
+    //----------Field
+    int column_;
+    int row_;
+    vector<vector<double> > matrix_;
+
+    //-----------Methods
+    void getCofactor_(Matrix matrix1, Matrix &tempMatrix, int p, int q, int n);
+    void openFile_ (string address);
+    double determinant_ (int n);
+    void adjoint_ (Matrix &adjoint);
+
+public:
+    //----------Constructor
+    Matrix (string address);
+    Matrix (int row, int column);
+    //---------Friends Methods-----------
+    friend istream &operator >> (istream &in , Matrix &matrix1);
+    friend  Matrix operator * (int const coefficient, Matrix const matrix) ;
+    //--------Public Methods------------------
+    void output();
+    Matrix operator + (Matrix const &matrix);
+    Matrix operator - (Matrix const matrix);
+    Matrix operator * (int const coefficient);
+    Matrix operator * (Matrix const &matrix);
+    bool operator == (Matrix const matrix);
+    Matrix transpose();
+    double determinant ();
+    void save (string name);
+    int getRow();
+    int getColumn();
+    string size();
+    Matrix inverse();
+};
+
+#endif //MATRIXLIBFINAL_LIBRARY_H
